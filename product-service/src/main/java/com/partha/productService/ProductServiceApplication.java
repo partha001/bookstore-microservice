eb3afa702ba2573df76537a25fea0d2800fd266f
Partha Biswas <biswas.partha1@gmail.com>
2018-06-14 19:24:21 +0530

    backend : other backend changes

--- product-service/src/main/java/com/partha/productService/ProductServiceApplication.java
+++ product-service/src/main/java/com/partha/productService/ProductServiceApplication.java
@@ -3,6 +3,9 @@ package com.partha.productService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,4 +14,12 @@ public class ProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
+	
+	
+
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
 }
--- product-service/src/main/java/com/partha/productService/controller/TestController.java
+++ product-service/src/main/java/com/partha/productService/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.partha.productService.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
+	private Logger logger = LoggerFactory.getLogger(TestController.class);
+	
 	@Value("${test.key1}")
 	private String key1;
 	
 	@GetMapping(value="/test")
 	public String test(){
+		logger.info("TestController.test() :: start"); 
 		return  key1;
 	}
 
