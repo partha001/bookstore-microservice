db2dbaf744a6647d81045a8ed36fefb6567bad21
partha011 <biswas.partha1@gmail.com>
2019-04-18 17:03:10 +0530

    backend: product service  - book entity updated

--- product-service/src/main/java/com/partha/productService/entities/Book.java
+++ product-service/src/main/java/com/partha/productService/entities/Book.java
@@ -1,13 +1,17 @@
 package com.partha.productService.entities;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,6 +38,9 @@ public class Book {
 	private String title;
 	
 	@Column(name="author")
+	private String author;
+	
+	@Column(name="category")
 	private String category;
 	
 	@Column(name="availableUnits")
@@ -41,5 +48,44 @@ public class Book {
 	
 	@Column(name="price")
 	private BigDecimal price;
+	
+	@Column(name="active")
+	private Boolean active;
+		
+	@Temporal(TemporalType.TIMESTAMP) 
+	@Column(name="insertDate")
+	private Date insertDate;
+	
+	@Temporal(TemporalType.TIMESTAMP) 
+	@Column(name="updateDate")
+	private Date updateDate;
+	
+	
+	@Column(name="publisher")
+	private String publisher;
+	
+	@Column(name="language")
+	private String language;
+	
+	@Column(name="pages")
+	private Integer pages;
+	
+	@Column(name="isbn")
+	private String isbn;
+	
+	@Column(name="format")
+	private String format;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Temporal(TemporalType.TIMESTAMP) 
+	@Column(name="publicationDate")
+	private Date publicationDate;
+	
+	@Lob
+	@Column(name="image",columnDefinition="mediumblob")
+	private byte[] image;
+	
 	
 }
