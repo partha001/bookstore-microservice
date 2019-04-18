29a171d4fbc2c494651a501ddba795cc7784a44a
partha011 <biswas.partha1@gmail.com>
2019-04-18 17:01:09 +0530

    backend: productService swagger config

--- product-service/src/main/java/com/partha/productService/config/SwaggerConfig.java
+++ product-service/src/main/java/com/partha/productService/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api(){
 		return new Docket(DocumentationType.SWAGGER_2)
-				.select().apis(RequestHandlerSelectors.basePackage("com.partha.productService.controllers"))
+				.select().apis(RequestHandlerSelectors.basePackage("com.partha.productService.controller"))
 				.build();
 		
 //        return new Docket(DocumentationType.SWAGGER_2)
