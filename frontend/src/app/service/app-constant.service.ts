8f1df05fa852e5b17cd3192bcdc00ac9d3683bf3
partha011 <biswas.partha1@gmail.com>
2019-04-25 09:43:18 +0530

    frontend: port change

--- frontend/src/app/service/app-constant.service.ts
+++ frontend/src/app/service/app-constant.service.ts
@@ -3,8 +3,8 @@ import { Injectable,Component } from '@angular/core';
 @Injectable()
 export class AppConstant {
 
-    public  SERVICE_ENDPOINT : string='http://localhost:8083';
-    public  SERVICE_ENDPOINT_API : string='http://localhost:8083/api';
+    public  SERVICE_ENDPOINT : string='http://localhost:31111';
+    public  SERVICE_ENDPOINT_API : string= this.SERVICE_ENDPOINT + '/api';
     public  USER_SERVICE_ENDPOINT : string = this.SERVICE_ENDPOINT_API+"/userService";
     public  PRODUCT_SERVICE_ENDPOINT : string = this.SERVICE_ENDPOINT_API + "productService"; 
 
