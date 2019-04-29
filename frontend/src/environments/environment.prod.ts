30dd6abcd2357de1e759167a97e8f679a7a40cfb
Partha Biswas <biswas.partha1@gmail.com>
2019-04-29 14:37:12 +0530

    frontend : environment related changes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  service_endpoint :'http://localhost:30083' 
+  //service_endpoint :'http://localhost:30083'
+  service_endpoint : 'http://gateway-service:30083' 
 };
