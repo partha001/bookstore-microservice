956e67b7f33b33c0c3cdad030e093e38235be778
partha011 <biswas.partha1@gmail.com>
2019-05-08 12:55:43 +0530

    devops changes : frontend backend integration

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //service_endpoint :'http://localhost:30083'
-  service_endpoint : 'http://gateway-service:30083' 
+  service_endpoint :'http://localhost:30083'
+  //service_endpoint : 'http://gateway-service:30083' 
 };
